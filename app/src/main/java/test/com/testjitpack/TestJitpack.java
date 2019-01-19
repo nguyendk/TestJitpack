0a648ed7ff391b5780a78dc5e22a539b99951909
nguyendk <nguyendk@segu.vn>
2019-01-19 16:04:15 +0700

    init project

---
+++ app/src/main/java/test/com/testjitpack/TestJitpack.java
@@ -0,0 +1,9 @@
+package test.com.testjitpack;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nguyendk on 1/19/2019.
+ */
+public class TestJitpack extends AppCompatActivity {
+}
